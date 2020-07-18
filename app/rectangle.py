ff2ee449e4f9b548c85955ba6b2c6868ba149b77
veetoki <vinhquan.1996@gmail.com>
2020-07-18 11:41:11 +0700

    First commit

---
+++ app/rectangle.py
@@ -0,0 +1,113 @@
+import random
+import pandas as pd
+from sqlalchemy import Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class Rectangle(Base):
+    __tablename__ = 'rectangle'
+
+    rectangle_id = Column(Integer, primary_key=True)
+    a = Column(Integer, nullable=False)
+    b = Column(Integer, nullable=False)
+    area = Column(Integer, nullable=True)
+    perimeter = Column(Integer, nullable=True)
+
+
+class RectangleOperations:
+    engine = create_engine('sqlite:///rectangledb.db')
+
+    def calc_area(self, a, b):
+        return a * b
+
+    def calc_perimeter(self, a, b):
+        return (a + b) * 2
+
+    def is_empty(self):
+        if not self.engine.dialect.has_table(self.engine, 'Rectangle'):
+            self.create_database_scheme()
+        session = self.get_session()
+        res = False if session.query(Rectangle).first() else True
+        session.close()
+        return res
+
+    def get_session(self):
+        return sessionmaker(bind=self.engine)()
+
+    def create_database_scheme(self):
+        Base.metadata.create_all(self.engine)
+
+    def insert_test_data(self):
+        session = self.get_session()
+        vals = []
+        for i in range(20):
+            a = random.randint(1, 100)
+            b = random.randint(1, 100)
+            rect = Rectangle(
+                a=a,
+                b=b,
+            )
+            vals.append(rect)
+
+        session.add_all(vals)
+        session.commit()
+        session.close()
+
+    def update_rectangle(self, rectangle_id):
+        session = self.get_session()
+
+        rectangle = session.query(Rectangle).filter(
+            Rectangle.rectangle_id == rectangle_id
+        )
+
+        rectangle_data = rectangle.one()
+
+        a = rectangle_data.a
+        b = rectangle_data.b
+
+        area = self.calc_area(a, b)
+        perimeter = self.calc_perimeter(a, b)
+
+        vals = {
+            'area': area,
+            'perimeter': perimeter
+        }
+        rectangle.update(vals)
+
+        data = [[
+            rectangle_data.rectangle_id,
+            rectangle_data.a,
+            rectangle_data.b,
+            rectangle_data.area,
+            rectangle_data.perimeter
+        ]]
+
+        session.commit()
+        session.close()
+
+        return data
+
+    def show_rectangle_data(self, data=[]):
+        if not data:
+            session = self.get_session()
+            rectangles = session.query(Rectangle).all()
+
+            data = [
+                [rectangle.rectangle_id,
+                 rectangle.a,
+                 rectangle.b,
+                 rectangle.area,
+                 rectangle.perimeter] for rectangle in rectangles]
+
+            session.commit()
+            session.close()
+
+        data_frame = pd.DataFrame(
+            data,
+            columns=['rectangle_id', 'a', 'b', 'area', 'perimeter'],
+        )
+        print(data_frame.to_string(index=False))
---
+++ app/task.py
@@ -0,0 +1,37 @@
+import logging
+from celery import Celery
+from rectangle import RectangleOperations
+
+
+app = Celery(
+    'task',
+    broker='redis://redis:6379/0',
+    backend='redis://redis:6379/0'
+)
+
+operations = RectangleOperations()
+
+
+@app.task
+def update_rectangle(rectangle_id):
+    rectangle_data = operations.update_rectangle(rectangle_id)
+    return rectangle_data
+
+
+if __name__ == '__main__':
+    if operations.is_empty():
+        operations.create_database_scheme()
+        operations.insert_test_data()
+
+    while True:
+        print('*' * 100)
+        print('List of Rectangles: ')
+        operations.show_rectangle_data()
+
+        rect_id = int(input('Enter the rectangle id: '))
+        result = update_rectangle.delay(rect_id)
+
+        data = result.get()
+
+        print('Updated Rectangle: ')
+        operations.show_rectangle_data(data)
